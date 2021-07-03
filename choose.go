6bfc6db147ccb89f3a3afde75263a079ce9e5972
bolazec <mark.bolazec@gmail.com>
2021-07-03 20:17:23 +0200

    Get working with a recent tip.

--- choose.go
+++ choose.go
@@ -1,27 +1,9 @@
 package generic
 
-func ChooseString(b bool, t, f string) string {
+func If[T any](b bool, t, f T) T {
 	if b {
 		return t
 	} else {
 		return f
 	}
 }
-
-func ChooseAny(b bool, t, f interface{}) interface{} {
-	if b {
-		return t
-	} else {
-		return f
-	}
-}
-
-/*
-func ChooseG[T any](b bool, t, f T) T {
-	if b {
-		return t
-	} else {
-		return f
-	}
-}
-*/
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/code-bolazec/generic
+module 0.0.0.0/generic
 
-go 1.16
+go 1.18
